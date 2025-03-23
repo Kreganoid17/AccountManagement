80f3d5bf2d7ec34a1a79dc1a7055be5f52f42227
Kreganoid17 <chettykubeshan0@gmail.com>
2025-03-23 21:58:15 +0200

    refactored code

--- AccountManagementAPI/Constants/EmailConstants.cs
+++ AccountManagementAPI/Constants/EmailConstants.cs
@@ -3,8 +3,7 @@
     public static class EmailConstants
     {
         public static string senderName = "AccountManagement";
-        public static string senderEmail { get; set; } = "chettykubeshan0@gmail.com";
-
+        public static string senderEmail { get; set; } = "accountmanagement@gmail.com";
         public static string CreateSubject { get; set; } = "TRANSACTION CREATED";
         public static string UpdateSubject { get; set; } = "TRANSACTION UPDATED";
         public static string DeleteSubject { get; set; } = "TRANSACTION DELETED";
--- AccountManagementAPI/HelperServices/Email/Models/EmailModel.cs
+++ AccountManagementAPI/HelperServices/Email/Models/EmailModel.cs
@@ -2,7 +2,7 @@
 {
     public class EmailModel
     {
-        public string recieverName { get; set; } = "Account Management";            //note this will not be hardcoded values this will be passed in from the current user context
+        public string recieverName { get; set; } = "Jamie Nolan";            //note this will not be hardcoded values this will be passed in from the current user context
 
         public string recieverEmail { get; set; } = "jaime48@ethereal.email";      //note this will not be hardcoded values this will be passed in from the current user context
 
